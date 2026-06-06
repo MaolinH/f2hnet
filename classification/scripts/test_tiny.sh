ac06140ea6cc8e0e8ddc3215909cbb61557bcc9c
Maolin Huang <hmaolin2222@163.com>
2026-06-06 13:05:28 +0800

    Update test_tiny.sh for multi-process testing

--- classification/scripts/test_tiny.sh
+++ classification/scripts/test_tiny.sh
@@ -1,1 +1,7 @@
-torchrun 
+torchrun --standalone --nproc_per_node=8 \
+            --master_port 1235 \
+              test.py \
+                --cfg configs/F2hNet_tiny.yaml \
+                --data-path data/imagenet1k \
+                --batch-size 128 \
+                --test-checkpoint path/to/f2hnet_t.pth
